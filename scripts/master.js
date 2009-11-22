0b04c8a0cfe9ae1f36757d19963cd6b8b0a54cb9
Stefano J. Attardi <stefano@attardi.org>
2009-11-21 21:29:56 -0500

    Added comments

--- scripts/master.js
+++ scripts/master.js
@@ -1,11 +1,19 @@
 var db = 'http://verbamanent.cloudant.com:5984/main/';
 var allPosts = '_design/ordered/_view/date?callback=?&limit=20';
 
+$.prototype.use = function() {
+    return this.clone().removeClass('template').insertBefore(this);
+};
+
 $(function() {
     $.getJSON(db + allPosts, function(json) {
-        $.each(json.rows, function(i, post) {
-            $('#post').clone().removeAttr('id').insertBefore($('#post'))
-            .find('.body').html(post.value.body);
+        $.each(json.rows, function(i, row) {
+            var post = row.value;
+            var el = $('.post.template').use().find('.body:first').html(post.body).end();
+            $.each(post.comments, function(i, comment) {
+                console.log(comment.body);
+                el.find('.comment.template').use().find('.body:first').html(comment.body);
+            });
         });
     })
 });
